10db208abbd17216ad685bba7f4d58176c39d697
sudden.wikholm <sudden.wikholm@gmail.com>
2020-12-22 20:35:21 +0100

    Remove bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { StepModule } from './step/step.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
@@ -16,7 +15,6 @@ import { EffectsModule } from '@ngrx/effects';
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StepModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
